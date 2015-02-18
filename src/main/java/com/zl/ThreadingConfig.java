f55c08fe2342508c0f23270cb75eb2112f71fd9c
Zhi Li <lizhieffe@gmail.com>
2015-02-17 22:26:29 -0500

    refactoring

--- src/main/java/com/zl/ThreadingConfig.java
+++ src/main/java/com/zl/ThreadingConfig.java
@@ -2,24 +2,29 @@ package com.zl;
 
 import java.util.concurrent.Executor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import com.zl.utils.AppProperties;
-
 @Configuration
 @EnableAsync
 public class ThreadingConfig implements AsyncConfigurer {
 
+	@Value("${default-thread-pool.core-threads}")
+    private String corePoolSize;
+	
+	@Value("${default-thread-pool.max-threads}")
+	private String maxPoolSize;
+	
 	@Override
 	public Executor getAsyncExecutor() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(Integer.parseInt(AppProperties.getInstance().get("default-thread-pool.core-threads")));
-		executor.setMaxPoolSize(Integer.parseInt(AppProperties.getInstance().get("default-thread-pool.max-threads")));
-        executor.setThreadNamePrefix("com.zl.spring-default-thread-pool");
-        executor.initialize();
+    	executor.setCorePoolSize(Integer.parseInt(corePoolSize));
+    	executor.setMaxPoolSize(Integer.parseInt(maxPoolSize));
+    	executor.setThreadNamePrefix("com.zl.spring-default-thread-pool");
+    	executor.initialize();
         return executor;
 	}
 }
